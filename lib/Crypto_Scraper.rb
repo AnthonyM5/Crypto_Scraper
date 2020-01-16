53b3838a35e683221b85cc8fe7a1a3c2a7354837
Anthony Mai <anthony.mai530@gmail.com>
2020-01-16 01:15:03 +0000

    first commit

---
+++ lib/Crypto_Scraper.rb
@@ -0,0 +1,6 @@
+require "Crypto_Scraper/version"
+
+module CryptoScraper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/Crypto_Scraper/version.rb
@@ -0,0 +1,3 @@
+module CryptoScraper
+  VERSION = "0.1.0"
+end
